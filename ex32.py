045d536f949dcf8149348ef9277b6b750aebf2f0
Финце Эдик <fintse74@mail.ru>
2019-11-08 09:46:44 +0500

    Update ex32.py

--- ex32.py
+++ ex32.py
@@ -3,16 +3,16 @@
 
 import random
 import sys
-n = 24
+n = 25
 a = [random.randint(0, 100) for i in range(0,n)]
 print(a)
 
 if (len(a) % 2 == 1):
-    print("Укажите четное число элементов массива")
-    sys.exit(0)
+    end = n
+else:
+    end = n-1
 
 
-
-for i in range(0, len(a), 2):
+for i in range(0, end-1, 2):
     a[i], a[i + 1] = a[i + 1], a[i]
 print(a)
